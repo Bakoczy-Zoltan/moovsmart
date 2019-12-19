bf23a562e71598a120f3331f5e78ea6a866cfe9c
Bakoczy Zoltan <bakoczy.zoltan@gmail.com>
2019-12-19 22:08:34 +0100

    refact moving picture box in detail page

--- moovsmart/src/app/components/property-details/property-details.component.ts
+++ moovsmart/src/app/components/property-details/property-details.component.ts
@@ -1,24 +1,30 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
-import { Observable } from 'rxjs';
-import { isNull } from 'util';
 import { PropertyDetailsModel } from '../../models/propertyDetails.model';
 import { PropertyService } from '../../services/property.service';
 
 @Component({
-  selector: 'app-property-details',
-  templateUrl: './property-details.component.html',
-  styleUrls: ['./property-details.component.css']
+    selector: 'app-property-details',
+    templateUrl: './property-details.component.html',
+    styleUrls: ['./property-details.component.css'],
 })
 
 export class PropertyDetailsComponent implements OnInit {
 
 
-    propertyImage: any[];
+    defaultPicture = 'https://atasouthport.com/wp-content/uploads/2017/04/default-image.jpg';
     propertyDetails: PropertyDetailsModel;
+
+    images = ['https://www.cartoonnetworkhotel.com/sites/cnhotel.com/files/pcore_tiers/Cartoon%20Network%20Hotel_Room-AdventureTime.jpg',
+        'https://www.danubiushotels.com/w/accomms/0_1000/0/rooms/Palatinus-Economy-Room-midi1775.jpg',
+        'https://www.icehotel.com/sites/cb_icehotel/files/styles/image_column_large/public/Kaamos-Johan-Broberg.jpg?h=3c9275bd&itok=gHToh0qO'];
 
     constructor(private propertyService: PropertyService, private activatedRoute: ActivatedRoute,
                 private router: Router) {
+
+        this.defaultPicture = this.images[0];
+
+
         this.activatedRoute.paramMap.subscribe(
             paramMap => {
                 const idParam: number = +paramMap.get('id');
@@ -35,23 +41,12 @@ export class PropertyDetailsComponent implements OnInit {
             });
     }
 
-  ngOnInit() {
-  }
+    ngOnInit() {
+    }
 
-  openModal() {
 
-  }
-
-  currentSlide(i: number) {
-
-  }
-
-  closeModal() {
-
-  }
-
-  plusSlides(number: number) {
-
-  }
+    changeDefaultImg(image: string) {
+        this.defaultPicture = image;
+    }
 }
 
