b5a93439e4fa740aa992d423be97ade521e4660d
Érsek Ferenc <ersekferi79@gmail.com>
2019-12-19 14:20:37 +0100

    start property-details.component.ts

--- moovsmart/src/app/components/property-details/property-details.component.ts
+++ moovsmart/src/app/components/property-details/property-details.component.ts
@@ -1,4 +1,6 @@
 import {Component, OnInit} from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { PropertyService } from '../../services/property.service';
 
 @Component({
   selector: 'app-property-details',
@@ -6,11 +8,21 @@ import {Component, OnInit} from '@angular/core';
   styleUrls: ['./property-details.component.css']
 })
 export class PropertyDetailsComponent implements OnInit {
+
   propertyImage: any[];
+  propertyDetails: PropertyDetailsModel;
 
-
-
-  constructor() {
+  constructor(private propertyService: PropertyService, private activatedRoute: ActivatedRoute,
+              private router: Router) {
+    this.activatedRoute.paramMap.subscribe(
+        paramMap => {
+          const idParam: number = +paramMap.get('id');
+        if (isNaN(idParam) || idParam < 1) {
+          this.router.navigate(['property-list']);
+        } else {
+          this.propertyDetails = this.propertyService.getPropertyDetails(idParam);
+        }
+        });
   }
 
   ngOnInit() {
