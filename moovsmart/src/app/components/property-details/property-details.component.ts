e1b416ca587b9b614713beb64a41889e90e9bbe0
Bakoczy Zoltan <bakoczy.zoltan@gmail.com>
2019-12-19 14:18:43 +0100

    making ligthbox in detail page

--- moovsmart/src/app/components/property-details/property-details.component.ts
+++ moovsmart/src/app/components/property-details/property-details.component.ts
@@ -5,15 +5,33 @@ import {Component, OnInit} from '@angular/core';
   templateUrl: './property-details.component.html',
   styleUrls: ['./property-details.component.css']
 })
+
 export class PropertyDetailsComponent implements OnInit {
-  propertyImage: any[];
-
-
+  propertyImage : any[];
 
   constructor() {
+    const img1 = "https://q-cf.bstatic.com/images/hotel/max1024x768/195/195846764.jpg";
+    const img2 =  "https://r-cf.bstatic.com/images/hotel/max1024x768/151/151787518.jpg";
+
+    this.propertyImage = [img1, img2, img1];
   }
 
   ngOnInit() {
   }
 
+  openModal() {
+
+  }
+
+  currentSlide(i: number) {
+
+  }
+
+  closeModal() {
+
+  }
+
+  plusSlides(number: number) {
+
+  }
 }
