3221e2983d92db4b921fd9e415ab19b2659eea32
Érsek Ferenc <ersekferi79@gmail.com>
2019-12-19 14:30:51 +0100

    start property-details.component.ts
    fixed some details & typos

--- moovsmart/src/app/components/property-details/property-details.component.ts
+++ moovsmart/src/app/components/property-details/property-details.component.ts
@@ -1,54 +1,50 @@
-import {Component, OnInit} from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
+import { Observable } from 'rxjs';
+import { PropertyDetailsModel } from '../../models/propertyDetails.model';
 import { PropertyService } from '../../services/property.service';
 
 @Component({
-  selector: 'app-property-details',
-  templateUrl: './property-details.component.html',
-  styleUrls: ['./property-details.component.css']
+    selector: 'app-property-details',
+    templateUrl: './property-details.component.html',
+    styleUrls: ['./property-details.component.css'],
 })
 
 export class PropertyDetailsComponent implements OnInit {
 
-  propertyImage: any[];
-  propertyDetails: PropertyDetailsModel;
 
-  constructor(private propertyService: PropertyService, private activatedRoute: ActivatedRoute,
-              private router: Router) {
-    this.activatedRoute.paramMap.subscribe(
-        paramMap => {
-          const idParam: number = +paramMap.get('id');
-        if (isNaN(idParam) || idParam < 1) {
-          this.router.navigate(['property-list']);
-        } else {
-          this.propertyDetails = this.propertyService.getPropertyDetails(idParam);
+    propertyImage: any[];
+    propertyDetails: Observable<PropertyDetailsModel>;
+
+    constructor(private propertyService: PropertyService, private activatedRoute: ActivatedRoute,
+                private router: Router) {
+      this.activatedRoute.paramMap.subscribe(
+          paramMap => {
+            const idParam: number = +paramMap.get('id');
+            if (isNaN(idParam) || idParam < 1) {
+              this.router.navigate(['property-list']);
+            } else {
+              this.propertyDetails = this.propertyService.getPropertyDetails(idParam);
+            }
+          });
+    }
+
+      ngOnInit(): void {
+        throw new Error("Method not implemented.");
+    }
+
+        openModal()
+        {
+
         }
-        });
-  propertyImage : any[];
 
-  constructor() {
-    const img1 = "https://q-cf.bstatic.com/images/hotel/max1024x768/195/195846764.jpg";
-    const img2 =  "https://r-cf.bstatic.com/images/hotel/max1024x768/151/151787518.jpg";
+        currentSlide(i: number){
+        }
 
-    this.propertyImage = [img1, img2, img1];
-  }
+        closeModal() {
+        }
 
-  ngOnInit() {
-  }
+        plusSlides(number: number){
+        }
+    }
 
-  openModal() {
-
-  }
-
-  currentSlide(i: number) {
-
-  }
-
-  closeModal() {
-
-  }
-
-  plusSlides(number: number) {
-
-  }
-}
