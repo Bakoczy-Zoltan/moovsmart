ae67ac9a975c0536feba8631022d9c9c4eae2994
Bakoczy Zoltan <bakoczy.zoltan@gmail.com>
2020-01-02 10:27:20 +0100

    short security testing, security still not work

--- src/test/java/com/progmasters/moovsmart/MoovsmartApplicationTest.java
+++ src/test/java/com/progmasters/moovsmart/MoovsmartApplicationTest.java
@@ -2,13 +2,11 @@ package com.progmasters.moovsmart;
 
 import com.progmasters.moovsmart.service.PropertyService;
 import com.progmasters.moovsmart.controller.PropertyController;
-import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnitRunner;
 
-import static org.mockito.Mockito.when;
 
 @RunWith(MockitoJUnitRunner.class)
 public class MoovsmartApplicationTest {
@@ -19,9 +17,5 @@ public class MoovsmartApplicationTest {
     @InjectMocks
     private PropertyController propertyController;
 
-    @Test
-    public void testAddProperty() {
-        when(propertyService.getProperties()).thenReturn(new );
 
-    }
 }
