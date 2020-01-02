6764abd2adbac36248a54d52502e9e0e70312180
Bakoczy Zoltan <bakoczy.zoltan@gmail.com>
2020-01-02 17:17:17 +0100

    Google map Api is added to the project. Need to customize it

--- moovsmart/src/app/components/property-details/property-details.component.ts
+++ moovsmart/src/app/components/property-details/property-details.component.ts
@@ -1,21 +1,30 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { PropertyDetailsModel } from '../../models/propertyDetails.model';
 import { PropertyService } from '../../services/property.service';
 
 @Component({
-  selector: 'app-property-details',
-  templateUrl: './property-details.component.html',
-  styleUrls: ['./property-details.component.css']
+    selector: 'app-property-details',
+    templateUrl: './property-details.component.html',
+    styleUrls: ['./property-details.component.css'],
 })
 
-export class PropertyDetailsComponent implements OnInit {
+export class PropertyDetailsComponent implements OnInit, AfterViewInit {
+
     defaultPicture = 'https://atasouthport.com/wp-content/uploads/2017/04/default-image.jpg';
     propertyDetails: PropertyDetailsModel;
     images: string[];
+    map: google.maps.Map;
+    lat = 47.587030;
+    lng = 19.045820;
+    coordinates: google.maps.LatLng;
+    mapOptions: google.maps.MapOptions;
+    marker: google.maps.Marker;
 
     constructor(private propertyService: PropertyService, private activatedRoute: ActivatedRoute,
                 private router: Router) {
+
+        this.coordinates = new google.maps.LatLng(this.lat, this.lng);
 
         this.activatedRoute.paramMap.subscribe(
             paramMap => {
@@ -40,7 +49,29 @@ export class PropertyDetailsComponent implements OnInit {
     }
 
     ngOnInit() {
+        this.mapOptions = {
+            center: this.coordinates,
+            zoom: 8,
+        };
     }
+
+    ngAfterViewInit() {
+        this.mapInitializer();
+    }
+
+    mapInitializer() {
+        this.map = new google.maps.Map(this.gmap.nativeElement,
+            this.mapOptions);
+
+        this.marker = new google.maps.Marker({
+            position: this.coordinates,
+            map: this.map,
+        });
+        this.marker.setMap(this.map);
+    }
+
+    @ViewChild('mapContainer', {static: false}) gmap: ElementRef;
+
 
     changeDefaultImg(image: string) {
         if (image !== undefined && image !== null) {
@@ -51,5 +82,6 @@ export class PropertyDetailsComponent implements OnInit {
     goBack() {
         this.router.navigate(['property-list']);
     }
+
 }
 
