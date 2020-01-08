949505cb2e63472c1359c21eaaba08cfd636a7b8
Polgardi Reka <polgardi@gmail.com>
2020-01-08 14:44:45 +0100

    regex

--- moovsmart/src/app/components/property-form/property-form.component.ts
+++ moovsmart/src/app/components/property-form/property-form.component.ts
@@ -127,6 +127,7 @@ export class PropertyFormComponent implements OnInit {
 //                  formData.isValid = true;
                   formData.imageUrl =  'https://res.cloudinary.com/demo/image/upload/' + data + '.jpg';
                   this.selectedFile = null;
+                  formData.owner = this.propertyService.userName;
                   this.propertyId ? this.updateProperty(formData) : this.createNewProperty(formData);
               },
               () => {}
